4f188cd3534b25b793f4c0d8a5f8edb342f109cc
AnthonyEbert <anthonyebert@gmail.com>
2020-08-19 14:13:42 +0200

    Success!!!!

--- stan/MU1.R
+++ stan/MU1.R
@@ -1,5 +1,15 @@
 
 library(rstan)
+library(parallel)
+options(mc.cores = parallel::detectCores())
+library(queuecomputer)
+
+sessionInfo()
+
+seed <- 1
+set.seed(seed)
+
+cl <- makeCluster(parallel::detectCores())
 
 theta1 = 2
 theta2 = 7
@@ -17,8 +27,9 @@ fit <- stan(
   file = "stan/MU1.stan",
   model_name = "example",
   data = dat,
-  iter = 50,
-  init = list(list(x = a[-n], trans_last_x = exp(-a[n]))),
-  chains = 1,
+  iter = 500,
+  init = rep(list(list(x = a[-n], trans_last_x = exp(-theta3 * a[n]), theta1 = theta1, theta2 = theta2, theta3 = theta3)), parallel::detectCores()),
+  chains = parallel::detectCores(),
+  cores = parallel::detectCores(),
   pars = c("theta1", "theta2", "theta3")
 )
